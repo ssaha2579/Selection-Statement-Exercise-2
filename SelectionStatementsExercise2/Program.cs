2a33db0bc7258933adfd3c2c0163ee7e9ebf30f1
Sagar Saha <Sagarsaha2579@gmail.com>
2025-08-25 23:28:50 -0400

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject(); 
+        }
+
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("what is your favroite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is tough");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is challenging since math involved");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is fun, History involves real world facts");
+                    break;
+
+                case "compsci":
+                    Console.WriteLine("Computer Science Rules!");
+                    break;
+
+                case "music":
+                    Console.WriteLine("love making and producing, working to be the best in the business"); 
+                    break;
+
+                default:
+                    Console.WriteLine("Cool,I couldn't agree any less"); 
+                    break;
+            }
         }
     }
 }
--- SelectionStatementsExercise2/obj/Debug/net9.0/SelectionStatementsExercise2.AssemblyInfo.cs
+++ SelectionStatementsExercise2/obj/Debug/net9.0/SelectionStatementsExercise2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd38283a868e698a5a43b3d83b36f30b7dcc422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581dc19c0f4a0bed99ad22d4db0c6c46a40b11c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
